8cd07989c151362d73a61786247e8a9974609808
janithharshana2000 <lucifergamingsl2021@gmail.com>
2023-03-14 16:59:15 +0530

    db

---
+++ app/src/main/java/adapters/TodoAdapter.kt
@@ -0,0 +1,17 @@
+package adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 0;
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        }
+    }
